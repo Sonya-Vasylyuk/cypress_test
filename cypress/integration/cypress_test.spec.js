329fc466f763d3f8c959ac98955a5828cc5555a3
Sonya-Vasylyuk <sonyavasylyuk@gmail.com>
2021-05-11 20:43:52 +0300

    advanced level

--- cypress/integration/cypress_test.spec.js
+++ cypress/integration/cypress_test.spec.js
@@ -1,8 +1,26 @@
+describe('Cypress test', () => {
+  let user;
 
-  it('clicks the link "type"', () => {
-    cy.visit('https://react-redux.realworld.io/#/login?_k=zhqe49');
-    cy.get(':nth-child(1) > .form-control').type('rfrfrfrfrtyur@gmail.com').should('have.value', 'rfrfrfrfrtyur@gmail.com');
-    cy.get(':nth-child(2) > .form-control').type('12345678').should('have.value', '12345678');
-    cy.get('.btn').click();
-    cy.get(':nth-child(4) > .nav-link').should('contain', 'r56789')
-  })
+    before(function () {
+        cy.task("freshUser").then((object) => {
+            user = object;
+        });
+    });
+
+      it('clicks the link "type"', () => {
+        cy.visit('https://react-redux.realworld.io/#/login?_k=zhqe49');
+        cy.get(':nth-child(1) > .form-control').type('rfrfrfrfrtyur@gmail.com').should('have.value', 'rfrfrfrfrtyur@gmail.com');
+        cy.get(':nth-child(2) > .form-control').type('12345678').should('have.value', '12345678');
+        cy.get('.btn').click();
+        cy.get(':nth-child(4) > .nav-link').should('contain', 'r56789')
+      })
+    
+      it('clicks the link "type"', () => {
+        cy.visit('https://react-redux.realworld.io/#/register?_k=mnjkk0');
+        cy.get(':nth-child(1) > .form-control').type(user.username).should('have.value', user.username);
+        cy.get(':nth-child(2) > .form-control').type(user.email).should('have.value', user.email);
+        cy.get(':nth-child(3) > .form-control').type('12345678').should('have.value', '12345678');
+        cy.get('.btn').click();
+        cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
+      })
+})
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,18 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+const faker = require("faker");
+
+module.exports = (on, config) => {
+    on("task", {
+        freshUser() {
+            user = {
+                username: faker.name.firstName(),
+                email: faker.internet.email(),
+                password: "12345678",
+            };
+            return user;
+        },
+    });
+};
