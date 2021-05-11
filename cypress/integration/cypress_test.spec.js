bed4a874088e267b744a66397746a1d0909c93b5
Sonya-Vasylyuk <sonyavasylyuk@gmail.com>
2021-05-11 20:03:53 +0300

    basic level

--- cypress/integration/cypress_test.spec.js
+++ cypress/integration/cypress_test.spec.js
@@ -1,5 +1,8 @@
 
   it('clicks the link "type"', () => {
-    cy.visit('https://react-redux.realworld.io/#/login?_k=zhqe49')
-
+    cy.visit('https://react-redux.realworld.io/#/login?_k=zhqe49');
+    cy.get(':nth-child(1) > .form-control').type('rfrfrfrfrtyur@gmail.com').should('have.value', 'rfrfrfrfrtyur@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('12345678').should('have.value', '12345678');
+    cy.get('.btn').click();
+    cy.get(':nth-child(4) > .nav-link').should('contain', 'r56789')
   })
